4b1e47c1befe443e4a6db92d591be5145a186963
Michele Pagano <75363823+AlmondFlame1287@users.noreply.github.com>
2021-04-22 12:59:29 +0200

    Varie modifiche

---
+++ esercitazioni/script.js
@@ -0,0 +1,11 @@
+function funzione() {
+    document.getElementById("spazio_cognome").style.color = "red";
+}
+
+function funzione2() {
+  document.getElementById("button").style.color = "blue";
+}
+
+function funzione3() {
+  document.getElementById("button").style.color = "black";
+}
